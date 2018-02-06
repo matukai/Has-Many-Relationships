70ce8fdb047fb08eec5c47c9b135fc4b8160a9a6
matukai <matt.tanji@gmail.com>
2018-02-05 19:29:10 -1000

    table created

---
+++ has_many_blogs.sql
@@ -0,0 +1,46 @@
+
+
+-- DROP USER IF EXISTS has_many_user;
+
+CREATE USER has_many_user;
+
+-- DROP DATABASE IF EXISTS has_many_blogs;
+
+CREATE DATABASE has_many_blogs OWNER has_many_user;
+
+\c has_many_blogs 
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id     SERIAL  PRIMARY KEY           NOT NULL,
+  username    VARCHAR(90)              NOT NULL,
+  first_name  VARCHAR(90)              NULL,
+  last_name   VARCHAR(90)              NULL      DEFAULT NULL,
+  created_at  timestamp with time zone NOT NULL  DEFAULT now(),
+  updated_at  timestamp with time zone NOT NULL  DEFAULT now()
+);
+
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE posts (
+  id     SERIAL          PRIMARY KEY       ,
+  title   VARCHAR(180)                       DEFAULT NULL,
+  url      VARCHAR(510)                         DEFAULT NULL,
+  content     text                             DEFAULT NULL,
+  created_at timestamptz NOT NULL DEFAULT now(),
+  updated_at timestamptz NOT NULL DEFAULT now(),
+  users_id integer REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS comments;
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  body VARCHAR(510) NULL DEFAULT NULL,
+  created_at timestamptz NOT NULL DEFAULT now(),
+  updated_at timestamptz NOT NULL DEFAULT now(),
+  users_id integer REFERENCES users(id),
+  posts_id integer REFERENCES posts(id)
+);
+
+\i scripts/blog_data.sql
